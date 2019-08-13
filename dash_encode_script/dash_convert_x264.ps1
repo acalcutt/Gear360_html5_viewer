1c6715e12a4a4933faf010024da1b0781e9df140
acalcutt <acalcutt@techidiots.net>
2019-08-13 08:24:09 -0400

    added quotes around filename so paths with space work

--- dash_encode_script/dash_convert_x264.ps1
+++ dash_encode_script/dash_convert_x264.ps1
@@ -1,5 +1,5 @@
-﻿$input = Read-Host 'What file do you want to convert?'
-#$input = "X:\360_0067.MP4"
+﻿#$input = Read-Host `"What file do you want to convert?`"
+$input = "M:\360 Videos\Origional Files\ATV\2019-06-15 - Byron ME\360_0029.MP4"
 
 $starttime = ""
 $endtime = ""
@@ -25,51 +25,51 @@ If(Test-Path $input)
     $outfile_basename = "$($output_folder)$($basename)"
 
     $thumbnail_name = "$($output_folder)thumbnail.jpg"
-    $params8 = "$($starttime)-i $input -qscale:v 4 -vframes 1 $thumbnail_name"
+    $params8 = "$($starttime)-i `"$input`" -qscale:v 4 -vframes 1 `"$thumbnail_name`""
     Write-Host "Creating $thumbnail_name | ffmpeg.exe $params8"
     if (-Not (Test-Path $thumbnail_name)) {Start-Process -Wait -FilePath "ffmpeg.exe" -ArgumentList "$params8"}
 
     #Create Faillback MP4 (x264/AAC), for use when dash is not supported
 
     $outfile0 = "$($output_folder)fallback.mp4"
-    $params0 = "$($hwaccel_PARAMS)$($starttime)-i $input $($endtime)-c:v libx264 -s 1280x720 -b:v 2000k $x264_DASH_PARAMS -c:a aac -b:a 128k -f mp4 -dash 1 $outfile0"
+    $params0 = "$($hwaccel_PARAMS)$($starttime)-i `"$input`" $($endtime)-c:v libx264 -s 1280x720 -b:v 2000k $x264_DASH_PARAMS -c:a aac -b:a 128k -f mp4 -dash 1 `"$outfile0`""
     Write-Host "Creating $outfile0 | ffmpeg.exe $params0"
     if (-Not (Test-Path $outfile0)) {Start-Process -Wait -FilePath "ffmpeg.exe" -ArgumentList "$params0"}
 
     #Create several different resolutions at different bitrates using ffmpeg
 
     $outfile1 = "$($outfile_basename)_640x360_750k.mp4"
-    $params1 = "$($hwaccel_PARAMS)$($starttime)-i $input $($endtime)-c:v libx264 -s 640x360 -b:v 750k $x264_DASH_PARAMS -an -f mp4 -dash 1 $outfile1"
+    $params1 = "$($hwaccel_PARAMS)$($starttime)-i `"$input`" $($endtime)-c:v libx264 -s 640x360 -b:v 750k $x264_DASH_PARAMS -an -f mp4 -dash 1 `"$outfile1`""
     Write-Host "Creating $outfile1 | ffmpeg.exe $params1"
     if (-Not (Test-Path $outfile1)) {Start-Process -Wait -FilePath "ffmpeg.exe" -ArgumentList "$params1"}
 
     $outfile2 = "$($outfile_basename)_896x504_1000k.mp4"
-    $params2 = "$($hwaccel_PARAMS)$($starttime)-i $input $($endtime)-c:v libx264 -s 896x504 -b:v 1000k $x264_DASH_PARAMS -an -f mp4 -dash 1 $outfile2"
+    $params2 = "$($hwaccel_PARAMS)$($starttime)-i `"$input`" $($endtime)-c:v libx264 -s 896x504 -b:v 1000k $x264_DASH_PARAMS -an -f mp4 -dash 1 `"$outfile2`""
     Write-Host "Creating $outfile2 | ffmpeg.exe $params2"
     if (-Not (Test-Path $outfile2)) {Start-Process -Wait -FilePath "ffmpeg.exe" -ArgumentList "$params2"}
 
     $outfile3 = "$($outfile_basename)_1280x720_2500k.mp4"
-    $params3 = "$($hwaccel_PARAMS)$($starttime)-i $input $($endtime)-c:v libx264 -s 1280x720 -b:v 2500k $x264_DASH_PARAMS -an -f mp4 -dash 1 $outfile3"
+    $params3 = "$($hwaccel_PARAMS)$($starttime)-i `"$input`" $($endtime)-c:v libx264 -s 1280x720 -b:v 2500k $x264_DASH_PARAMS -an -f mp4 -dash 1 `"$outfile3`""
     Write-Host "Creating $outfile3 | ffmpeg.exe $params3"
     if (-Not (Test-Path $outfile3)) {Start-Process -Wait -FilePath "ffmpeg.exe" -ArgumentList "$params3"}
 
     $outfile4 = "$($outfile_basename)_1280x720_5000k.mp4"
-    $params4 = "$($hwaccel_PARAMS)$($starttime)-i $input $($endtime)-c:v libx264 -s 1280x720 -b:v 5000k $x264_DASH_PARAMS -an -f mp4 -dash 1 $outfile4"
+    $params4 = "$($hwaccel_PARAMS)$($starttime)-i `"$input`" $($endtime)-c:v libx264 -s 1280x720 -b:v 5000k $x264_DASH_PARAMS -an -f mp4 -dash 1 `"$outfile4`""
     Write-Host "Creating $outfile4 | ffmpeg.exe $params4"
     if (-Not (Test-Path $outfile4)) {Start-Process -Wait -FilePath "ffmpeg.exe" -ArgumentList "$params4"}
 
     $outfile5 = "$($outfile_basename)_1920x1080_7500k.mp4"
-    $params5 = "$($hwaccel_PARAMS)$($starttime)-i $input $($endtime)-c:v libx264 -s 1920x1080 -b:v 7500k $x264_DASH_PARAMS -an -f mp4 -dash 1 $outfile5"
+    $params5 = "$($hwaccel_PARAMS)$($starttime)-i `"$input`" $($endtime)-c:v libx264 -s 1920x1080 -b:v 7500k $x264_DASH_PARAMS -an -f mp4 -dash 1 `"$outfile5`""
     Write-Host "Creating $outfile5 | ffmpeg.exe $params5"
     if (-Not (Test-Path $outfile5)) {Start-Process -Wait -FilePath "ffmpeg.exe" -ArgumentList "$params5"}
 
     $outfile6 = "$($outfile_basename)_1920x1080_10000k.mp4"
-    $params6 = "$($hwaccel_PARAMS)$($starttime)-i $input $($endtime)-c:v libx264 -s 1920x1080 -b:v 10000k $x264_DASH_PARAMS -an -f mp4 -dash 1 $outfile6"
+    $params6 = "$($hwaccel_PARAMS)$($starttime)-i `"$input`" $($endtime)-c:v libx264 -s 1920x1080 -b:v 10000k $x264_DASH_PARAMS -an -f mp4 -dash 1 `"$outfile6`""
     Write-Host "Creating $outfile6 | ffmpeg.exe $params6"
     if (-Not (Test-Path $outfile6)) {Start-Process -Wait -FilePath "ffmpeg.exe" -ArgumentList "$params6"}
 
     $outfile7 = "$($outfile_basename)_audio_128k.mp4"
-    $params7 = "-i $input $($starttime)$($endtime)-c:a aac -b:a 128k -vn $outfile7"
+    $params7 = "-i `"$input`" $($starttime)$($endtime)-c:a aac -b:a 128k -vn `"$outfile7`""
     Write-Host "Creating $outfile7 | ffmpeg.exe $params7"
     if (-Not (Test-Path $outfile7)) {Start-Process -Wait -FilePath "ffmpeg.exe" -ArgumentList "$params7"}
 
@@ -78,7 +78,7 @@ If(Test-Path $input)
     #Create the dash manifest file
 
     $manifest_name = "$($output_folder)Play.mpd"
-    $params9 = "-dash 2000 -rap -frag-rap -profile onDemand $outfile1 $outfile2 $outfile3 $outfile4 $outfile5 $outfile6 $outfile7 -out $manifest_name"
+    $params9 = "-dash 2000 -rap -frag-rap -profile onDemand `"$outfile1`" `"$outfile2`" `"$outfile3`" `"$outfile4`" `"$outfile5`" `"$outfile6`" `"$outfile7`" -out `"$manifest_name`""
     Write-Host "Creating | MP4Box.exe $params9"
     if (-Not (Test-Path $manifest_name)) {Start-Process -Wait -FilePath "MP4Box.exe" -ArgumentList "$params9"}
 
